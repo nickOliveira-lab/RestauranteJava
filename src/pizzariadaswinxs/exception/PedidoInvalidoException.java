9e15eebfa8d9311599e648eee918a49e923ca88e
nick-Oliver <nickplys.slira@gmail.com>
2026-08-29 22:20:24 -0300

    Adiciona o Package Exeption e PedidioInvalidoException

---
+++ src/pizzariadaswinxs/exception/PedidoInvalidoException.java
@@ -0,0 +1,10 @@
+package pizzariadaswinxs.exception;
+
+public class PedidoInvalidoException extends RuntimeException{
+
+    public PedidoInvalidoException(String mensagem){
+        super(mensagem);
+
+    }
+
+}
